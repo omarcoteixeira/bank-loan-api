59a5bf077289edf590e5c02d5027818cde9633ce
Marco Túlio Teixeira <tulioteixeira@google.com>
2018-02-14 16:11:33 -0200

    #4 Adding SQL support.

--- utils/db.py
+++ utils/db.py
@@ -16,12 +16,9 @@ def execute():
         port=url.port
     )
 
-    print(conn)
-
     cur = conn.cursor()
 
     cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-
     cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
     cur.execute("SELECT * FROM test;")
 
